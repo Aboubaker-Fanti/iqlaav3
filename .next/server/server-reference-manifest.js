780e84783e0140906ea15e43fc61b0269cfb96b8
Aboubaker fanti <fantiaboubaker@gmail.com>
2025-08-01 11:45:12 -0400

    fanti

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"g+5EnnQCtGmhMe/trvasZ644LJrZlCvuO78FmmopVxo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
